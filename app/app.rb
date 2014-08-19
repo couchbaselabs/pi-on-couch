9ec82749925361fef79fd7f380206a70b76eee09
Philipp Fehre <philipp.fehre@googlemail.com>
2014-08-19 17:05:30 +0100

    move replication_change_notifier service inline
    as the notifier features are no longer needed and the class is only
    logging this can be moved inline

--- app/app.rb
+++ app/app.rb
@@ -2,10 +2,11 @@ require "java"
 
 require "services/database"
 require "controllers/messages_controller"
-require "services/replication_change_notifier"
 
 module PiOnCouch
   class Application
+    include_package "com.couchbase.lite.replicator"
+
     def initialize sync_url
       Database.establish_connection "messages"
 
@@ -21,15 +22,26 @@ module PiOnCouch
       pull_rep = database.create_pull_replication(url)
       pull_rep.continuous = true
 
-      pull_listener = ReplicationChangeNotifier.new
-      pull_rep.add_change_listener pull_listener
+      replication_listener = Replication::ChangeListener.new
+      class << replication_listener
+        java_signature 'void changed(ChangeEvent event)'
+        def changed event
+          replication = event.source
+          if replication.last_error
+            $log.warn "replication encountered an error: #{replication.last_error}"
+          else
+            $log.debug "#{replication.getCompletedChangesCount} of #{replication.changes_count} changes replicated"
+          end
+        end
+      end
+
+      pull_rep.add_change_listener replication_listener
       pull_rep.start
 
       push_rep = database.create_push_replication(url)
       push_rep.continuous = true
 
-      push_listener = ReplicationChangeNotifier.new
-      push_rep.add_change_listener push_listener
+      push_rep.add_change_listener replication_listener
       push_rep.start
     end
   end
